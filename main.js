fc1e5bdf814cae1260b92b9a0ab80b946a94878e
DarioGuastella <jimmyguastella@gmail.com>
2023-11-20 17:45:51 +0100

    feat: bottone genera lista

--- main.js
+++ main.js
@@ -8,17 +8,19 @@ createApp({
         
     },
     methods: {
-        
+        generaEmails() {
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(resp => {
+                    
+                    if(resp.data.success) {
+                        this.emails.push(resp.data.response);
+                    }
+                });}
+            }
     },
     mounted() {
 
-        for (let i = 0; i < 10; i++) {
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(resp => {
-            
-            if(resp.data.success) {
-                this.emails.push(resp.data.response);
-            }
-        });}
+        
     }
 }).mount('#app')
 
